e782a2492108c24e3066e20e54ebd4f0dc772090
adarshsingh3384 <adarshsingh3384@gmail.com>
2026-06-12 22:14:54 +0530

    conversion of datatypes

---
+++ conversion.js
@@ -0,0 +1,22 @@
+let score = "abc123"
+
+let valueinnumber = Number(score)
+
+let valuein
+
+console.log(typeof(valueinnumber))
+console.log(valueinnumber)
+
+let name = 2123
+
+let valueinstring = String(name)
+
+console.log(typeof(valueinstring))
+console.log(valueinstring)
+
+let value = 1
+
+let valueinbollean = Boolean(value)
+
+console.log(typeof(valueinbollean))
+console.log(valueinbollean)
