609a7ef64f926cfb863b2c4398225f11be33818e
adarshsingh3384 <adarshsingh3384@gmail.com>
2026-06-14 12:55:16 +0530

    new

---
+++ compereision.js
@@ -0,0 +1,10 @@
+console.log(null >= 0)         //true
+console.log(null <= 0)         //true
+console.log(null == 0)         //false
+console.log(null != 0)         //true
+console.log(null < 0)          //false
+console.log(null > 0)          //false
+
+// strict check
+
+console.log("3" === 3)
---
+++ datatype-summery.js
@@ -0,0 +1,30 @@
+// Premitive
+
+// There are 7 types of datatype :-
+// string , number , bigint , boolean , null , undefined , symbol
+
+//symbol example 
+const id = Symbol('123')
+const valueid = Symbol('123')
+
+console.log(id == valueid)
+
+// Reference type (non permitive)
+
+// Array , Object , Function
+// Array
+
+const data = ["hello" , "gay" , "ide"];
+
+
+// function
+const myfunction = function() {
+    console.log("hello world")
+}
+
+// object 
+let myobj = {
+    name:  "Adarsh",
+    age:  23
+
+}
